ea9c4cc34ee421f1dab2ed020181b8202fb3e9b8
DStow <Dan_Stow@live.co.uk>
2022-07-18 20:19:42 +0100

    Setup card deck to populate self

--- FoxInTheCookieFactory/FoxInTheCookieFactory.GameModel.Tests/CardDeckTests.cs
+++ FoxInTheCookieFactory/FoxInTheCookieFactory.GameModel.Tests/CardDeckTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Linq;
 
 namespace FoxInTheCookieFactory.GameModel.Tests
 {
@@ -7,10 +8,29 @@ namespace FoxInTheCookieFactory.GameModel.Tests
     public class CardDeckTests
     {
         [TestMethod]
-        public void Constructor_CorrectNumberOfCards()
+        public void Constructor_InitilizeDeck_CorrectNumber()
         {
             var cardDeck = new CardDeck();
 
+            int result = 33;
+
+            Assert.AreEqual(result, cardDeck.Cards.Count);
+        }
+
+        [TestMethod]
+        public void Constructor_InitilizeDeck_CorrectSuitSplit()
+        {
+            var cardDeck = new CardDeck();
+
+            int result = 11;
+
+            var moonCards = cardDeck.Cards.Where(x => x.Suit == Enumeration.CardSuitEnum.Moon);
+            var bellCards = cardDeck.Cards.Where(x => x.Suit == Enumeration.CardSuitEnum.Bell);
+            var keyCards = cardDeck.Cards.Where(x => x.Suit == Enumeration.CardSuitEnum.Key);
+
+            Assert.AreEqual(result, moonCards.Count());
+            Assert.AreEqual(result, bellCards.Count());
+            Assert.AreEqual(result, keyCards.Count());
         }
     }
 }
--- FoxInTheCookieFactory/FoxInTheCookieFactory.GameModel/CardDeck.cs
+++ FoxInTheCookieFactory/FoxInTheCookieFactory.GameModel/CardDeck.cs
@@ -6,5 +6,23 @@ namespace FoxInTheCookieFactory.GameModel
 {
     public class CardDeck
     {
+        public List<Card> Cards { get; set; } = new List<Card>();
+
+        public CardDeck()
+        {
+            PopulateCardSet();
+        }
+
+        private void PopulateCardSet()
+        {
+            Cards = new List<Card>();
+
+            for(int i = 1; i < 12; i++)
+            {
+                Cards.Add(new Card(i, Enumeration.CardSuitEnum.Bell));
+                Cards.Add(new Card(i, Enumeration.CardSuitEnum.Key));
+                Cards.Add(new Card(i, Enumeration.CardSuitEnum.Moon));
+            }
+        }
     }
 }
