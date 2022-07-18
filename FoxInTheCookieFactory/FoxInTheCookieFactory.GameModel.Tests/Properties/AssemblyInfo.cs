4d17c1cede49bf76718f7dd9f6706567a124b5ce
DStow <Dan_Stow@live.co.uk>
2022-07-18 19:27:55 +0100

    Add tests project for GameModel

---
+++ FoxInTheCookieFactory/FoxInTheCookieFactory.GameModel.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FoxInTheCookieFactory.GameModel.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FoxInTheCookieFactory.GameModel.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("455fe6c5-5839-48c6-a163-8212e82273e0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
