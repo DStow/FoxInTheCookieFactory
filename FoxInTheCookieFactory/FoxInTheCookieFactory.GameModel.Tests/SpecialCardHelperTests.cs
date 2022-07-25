6459e8dd446c7226e433139a9b611e1cead596fc
DStow <Dan_Stow@live.co.uk>
2022-07-25 20:27:12 +0100

    Add special card helper and tests

---
+++ FoxInTheCookieFactory/FoxInTheCookieFactory.GameModel.Tests/SpecialCardHelperTests.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace FoxInTheCookieFactory.GameModel.Tests
+{
+    [TestClass]
+    public class SpecialCardHelperTests
+    {
+        [TestMethod]
+        public void GetMonarchReactionPlayableCards_Has1AndNormal_Select1AndNormal()
+        {
+            var monarch = new Card(11, Enumeration.CardSuitEnum.Bell);
+
+            Player p = new Player("");
+            var expectedCards = new Card[] { new Card(1, Enumeration.CardSuitEnum.Bell), new Card(10, Enumeration.CardSuitEnum.Bell) };
+            p.Hand.Add(expectedCards[0]);
+            p.Hand.Add(expectedCards[1]);
+            p.Hand.Add(new Card(5, Enumeration.CardSuitEnum.Bell));
+
+            p.Hand.Add(new Card(5, Enumeration.CardSuitEnum.Moon));
+            p.Hand.Add(new Card(9, Enumeration.CardSuitEnum.Key));
+
+            List<Card> results = SpecialCardHelper.GetMonarchReactionPlayableCards(p, monarch);
+
+            Assert.IsTrue(results.Contains(expectedCards[0]));
+            Assert.IsTrue(results.Contains(expectedCards[1]));
+        }
+
+        [TestMethod]
+        public void GetMonarchReactionPlayableCards_HasNormal_SelectNormal()
+        {
+            var monarch = new Card(11, Enumeration.CardSuitEnum.Bell);
+
+            Player p = new Player("");
+            var expectedCards = new Card[] { new Card(10, Enumeration.CardSuitEnum.Bell) };
+            p.Hand.Add(expectedCards[0]);
+            p.Hand.Add(new Card(5, Enumeration.CardSuitEnum.Bell));
+
+            p.Hand.Add(new Card(5, Enumeration.CardSuitEnum.Moon));
+            p.Hand.Add(new Card(9, Enumeration.CardSuitEnum.Key));
+
+            List<Card> results = SpecialCardHelper.GetMonarchReactionPlayableCards(p, monarch);
+
+            Assert.IsTrue(results.Contains(expectedCards[0]));
+        }
+
+        [TestMethod]
+        public void GetMonarchReactionPlayableCards_HasSwan_SelectSwan()
+        {
+            var monarch = new Card(11, Enumeration.CardSuitEnum.Bell);
+
+            Player p = new Player("");
+            var expectedCards = new Card[] { new Card(1, Enumeration.CardSuitEnum.Bell) };
+            p.Hand.Add(expectedCards[0]);
+
+            p.Hand.Add(new Card(5, Enumeration.CardSuitEnum.Moon));
+            p.Hand.Add(new Card(9, Enumeration.CardSuitEnum.Key));
+
+            List<Card> results = SpecialCardHelper.GetMonarchReactionPlayableCards(p, monarch);
+
+            Assert.IsTrue(results.Contains(expectedCards[0]));
+        }
+
+        [TestMethod]
+        public void GetMonarchReactionPlayableCards_HasNone_SelectHand()
+        {
+            var monarch = new Card(11, Enumeration.CardSuitEnum.Bell);
+
+            Player p = new Player("");
+
+            p.Hand.Add(new Card(5, Enumeration.CardSuitEnum.Moon));
+            p.Hand.Add(new Card(9, Enumeration.CardSuitEnum.Key));
+            p.Hand.Add(new Card(11, Enumeration.CardSuitEnum.Moon));
+            p.Hand.Add(new Card(6, Enumeration.CardSuitEnum.Key));
+            p.Hand.Add(new Card(10, Enumeration.CardSuitEnum.Key));
+
+            List<Card> results = SpecialCardHelper.GetMonarchReactionPlayableCards(p, monarch);
+
+            Assert.IsTrue(results.Count == 5);
+        }
+    }
+}
---
+++ FoxInTheCookieFactory/FoxInTheCookieFactory.GameModel/SpecialCardHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Linq;
+
+[assembly: InternalsVisibleTo("FoxInTheCookieFactory.GameModel.Tests")]
+namespace FoxInTheCookieFactory.GameModel
+{
+    internal static class SpecialCardHelper
+    {
+        public static List<Card> GetMonarchReactionPlayableCards(Player player, Card playedMonarch)
+        {
+            var playableCards = player.Hand.Where(x => x.Suit == playedMonarch.Suit && (x.Value == 1 || x.Value == player.Hand.Where(y => y.Suit == playedMonarch.Suit).Max(y => y.Value))).Distinct().ToList();
+
+            if (playableCards.Count == 0)
+                return player.Hand;
+            else
+                return playableCards;
+        }
+    }
+}
