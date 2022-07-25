6baff974cc4de004360b638e59d651a1de9d912d
DStow <Dan_Stow@live.co.uk>
2022-07-25 18:38:14 +0100

    Refactor game logic to loop until a winner

--- FoxInTheCookieFactory/FoxInTheCookieFactory.ConsoleApp/Program.cs
+++ FoxInTheCookieFactory/FoxInTheCookieFactory.ConsoleApp/Program.cs
@@ -12,15 +12,31 @@ namespace FoxInTheCookieFactory.ConsoleApp
         {
             var game = new GameModel.Game();
             game.Initilize("P1", "P2");
+            PlayRound(game);
 
-            while(game.HasGameEnded() == false)
+
+            // By being here the game has ended...
+            // Calculate scores
+            // Does a player meant the minimum win score limit (13?)
+
+
+
+        }
+
+        private static void PlayRound(GameModel.Game game)
+        {
+            while (game.HasGameEnded() == false)
             {
+                Console.ForegroundColor = ConsoleColor.Blue;
                 Console.WriteLine("Decree card: " + game.DecreeCard);
 
+                Console.WriteLine("");
+                Console.ForegroundColor = ConsoleColor.Green;
                 var lCard = GetPlayerPlayCard(game.LeadingPlayer);
                 game.PlayPlayerCard(game.LeadingPlayer, lCard, null);
                 Console.WriteLine();
 
+                Console.ForegroundColor = ConsoleColor.Red;
                 var fCard = GetPlayerPlayCard(game.FollowingPlayer);
                 game.PlayPlayerCard(game.FollowingPlayer, fCard, null);
                 Console.WriteLine();
@@ -34,15 +50,28 @@ namespace FoxInTheCookieFactory.ConsoleApp
             game.ScoreUpPlayers();
 
             // Check for winner
+            var winner = game.CheckForWinner();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine();
 
+            if (winner != null)
+            {
+                Console.WriteLine(winner.Name + " is the winner!");
+                Console.WriteLine(game.Player1.Name + ": " + game.Player1.TotalScore);
+                Console.WriteLine(game.Player2.Name + ": " + game.Player2.TotalScore);
+                Console.ReadLine();
+                return;
+            }
+            else
+            {
+                Console.WriteLine("No winner yet! Scores:");
+                Console.WriteLine(game.Player1.Name + ": " + game.Player1.TotalScore);
+                Console.WriteLine(game.Player2.Name + ": " + game.Player2.TotalScore);
 
+                game.ResetDeckAndDeal();
 
-            // By being here the game has ended...
-            // Calculate scores
-            // Does a player meant the minimum win score limit (13?)
-
-
-
+                PlayRound(game);
+            }
         }
 
         static GameModel.Card GetPlayerPlayCard(GameModel.Player player)
