abcc6a6fef9b4cdcb3a82e1e26581ae7a37d2501
DStow <Dan_Stow@live.co.uk>
2022-07-30 18:48:44 +0100

    Renaming of base android project files

--- FoxInTheCookieFactory/FoxInTheCookieFactory.Android/FoxGame.cs
+++ FoxInTheCookieFactory/FoxInTheCookieFactory.Android/FoxGame.cs
@@ -4,12 +4,12 @@ using Microsoft.Xna.Framework.Input;
 
 namespace FoxInTheCookieFactory.Android
 {
-    public class Game1 : Game
+    public class FoxGame : Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        public Game1()
+        public FoxGame()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
